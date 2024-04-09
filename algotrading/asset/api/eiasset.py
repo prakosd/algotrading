2b85c7a4b3e4f5fdea37c066ee8e86de2ff197dd
prakosd <prakosd@yahoo.co.id>
2024-04-09 22:40:58 +0700

    feat: add search Asset by code and type

--- algotrading/asset/api/eiasset.py
+++ algotrading/asset/api/eiasset.py
@@ -1,4 +1,4 @@
-"""Module of Symbol Entity Interface"""
+"""Module of Asset Entity Interface"""
 from dataclasses import dataclass, field
 from abc import ABC
 from typing import ClassVar
@@ -10,18 +10,22 @@ from ...common.asset import AssetType, AssetEnum
 
 @dataclass
 class EIAsset(ABC):
-    """Symbol Entity Interface"""
+    """Asset Entity Interface"""
     _DATA_DIR: ClassVar[str] = config.common.data_directory
     _FILE_EXT: ClassVar[str] = config.common.file_extension
     _FILENAME: ClassVar[str] = config.common.asset_filename
     _DATA: ClassVar[pd.DataFrame] = None
+    _CODE: ClassVar[str] = "CODE"
+    _TYPE: ClassVar[str] = "TYPE"
+    _NAME: ClassVar[str] = "NAME"
 
     code: AssetEnum
     type: AssetType = field(init=False)
     name: str = field(init=False)
 
     @staticmethod
-    def get_assets(reload: bool=False) -> pd.DataFrame:
+    def get_assets(code: AssetEnum=None, asset_type: AssetType=None,
+                   reload: bool=False) -> pd.DataFrame:
         """Return list of asset"""
         if EIAsset._DATA is None or reload:
             directory = EIAsset._DATA_DIR
@@ -32,15 +36,23 @@ class EIAsset(ABC):
             else:
                 raise FileNotFoundError(f"{directory + filename}")
 
-        return EIAsset._DATA
+        df = EIAsset._DATA.copy()
+
+        if code is not None:
+            df = df.loc[(df[EIAsset._CODE] == code.value)]
+
+        if asset_type is not None:
+            df = df.loc[(df[EIAsset._TYPE] == asset_type.value)]
+
+        return df
 
     def __post_init__(self):
         """Post initialization"""
         df = EIAsset.get_assets()
         try:
-            asset_code = df.loc[(df['CODE'] == self.code.value), 'TYPE'].iloc[0]
-            self.type = AssetType(asset_code)
-            self.name = df.loc[(df['CODE'] == self.code.value), 'NAME'].iloc[0]
+            asset_type = df.loc[(df[EIAsset._CODE] == self.code.value), EIAsset._TYPE].iloc[0]
+            self.type = AssetType(asset_type)
+            self.name = df.loc[(df[EIAsset._CODE] == self.code.value), EIAsset._NAME].iloc[0]
         except IndexError as error:
             print("An exception occurred:", error)
             print(f"AssetEnum: {self.code} doesn't exist")
