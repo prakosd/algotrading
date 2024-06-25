ba7aa8d942923c62f62541a15e4ad9fb045853d4
Danang Prakoso <danang.prakoso@verint.com>
2024-06-25 12:40:23 +1000

    fix: simplify buyandhold factory by passing ticker

--- algotrading/backtesting/strategies/factory.py
+++ algotrading/backtesting/strategies/factory.py
@@ -5,6 +5,7 @@ from typing import ClassVar
 
 from ...common.trade import Timeframe
 from ...common.asset import AssetPairCode as Symbol
+from ...ticker.ticker import Ticker
 from .buyandhold.implementation import BuyAndHoldStrategy
 from .contrarian.implementation import ContrarianStrategy
 from ...providers import ProviderFactory
@@ -16,17 +17,8 @@ class StrategyFactory():
     PROVIDER_OANDA: ClassVar[str] = ProviderFactory.OANDA
 
     @staticmethod
-    def create_buyandhold(provider_name: str, symbol: Symbol, start_date: dt,
-                          end_date: dt, timeframe: Timeframe,
-                          params: BuyAndHoldParams,
-                          force_download_ticker: bool=False) -> BuyAndHoldStrategy:
+    def create_buyandhold(ticker: Ticker, params: BuyAndHoldParams) -> BuyAndHoldStrategy:
         """Return new buyandhold strategy object"""
-        provider = ProviderFactory.create_provider_by_name(
-            provider_name, symbol, start_date, end_date, timeframe)
-        if provider is None:
-            return None
-
-        ticker = provider.get_ticker(force_download_ticker)
         if ticker is None or ticker.get_length() <= 0:
             return None
 
